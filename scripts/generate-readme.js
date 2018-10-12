606c600605038c1deaece2968a5b0d6361ab0151
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-12 21:04:12 +0300

    Fix ordering in generate-readme script

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -11,22 +11,20 @@ const OUTPUT_PATH = join(__dirname, '../README.md');
 
 (async () => {
   const packages = await getPackages();
-  // Ensure state is first
-  const sortedPackages = sortBy(packages, p => (p === 'state' ? -1 : 1));
 
-  let packageLinks = [];
-  let packageSections = [];
-
+  let pkgReadmes = {};
   await Promise.all(
-    sortedPackages.map(async pkg => {
+    packages.map(async pkg => {
       const pkgReadme = await readPackageReadme(pkg);
 
-      packageSections = [...packageSections, pkgReadme];
-      packageLinks = [...packageLinks, getLinkFromPkgReadme(pkgReadme)];
+      pkgReadmes = {
+        ...pkgReadmes,
+        [pkg]: pkgReadme
+      };
     })
   );
 
-  await writeMainReadme({ packageLinks, packageSections });
+  await writeMainReadme({ pkgReadmes });
 })();
 
 async function readPackageReadme(pkg) {
@@ -35,7 +33,21 @@ async function readPackageReadme(pkg) {
   return readFile(p, 'utf8');
 }
 
-async function writeMainReadme({ packageLinks, packageSections }) {
+async function writeMainReadme({ pkgReadmes }) {
+  // Ensure `state` is first
+  const pkgNames = sortBy(
+    Object.keys(pkgReadmes),
+    p => (p === 'state' ? -1 : 1)
+  );
+
+  let packageSections = [];
+  let packageLinks = [];
+
+  pkgNames.forEach(pkg => {
+    packageSections = [...packageSections, pkgReadmes[pkg]];
+    packageLinks = [...packageLinks, getLinkFromPkgReadme(pkgReadmes[pkg])];
+  });
+
   const template = await readFile(TEMPLATE_PATH, 'utf8');
   const output = template
     .replace(/\$PACKAGE_LINKS\n/g, packageLinks.map(l => `- ${l}`).join(`\n`))
