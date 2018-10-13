d48d1421fac6edf056b1e0c0db7974792148c5c7
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-13 19:12:53 +0300

    Disallow blank responses to new package script

--- scripts/new-package.js
+++ scripts/new-package.js
@@ -15,7 +15,8 @@ const TEMPLATE_PATH = join(__dirname, './templates/new-package');
     {
       type: 'input',
       name: 'pkgName',
-      message: `What's the package name?`
+      message: `What's the package name?`,
+      validate: val => val.length > 0
     }
   ]);
 
@@ -29,12 +30,14 @@ const TEMPLATE_PATH = join(__dirname, './templates/new-package');
     {
       type: 'input',
       name: 'pkgDesc',
-      message: `What's the package description?`
+      message: `What's the package description?`,
+      validate: val => val.length > 0
     },
     {
       type: 'input',
       name: 'compName',
-      message: `What's the component name?`
+      message: `What's the component name?`,
+      validate: val => val.length > 0
     }
   ]);
 
